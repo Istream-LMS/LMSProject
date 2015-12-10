7da4262ce57d5264a15f9eae7c209c7afb5ff30c
ramu12 <rammurthy.lingala@gmail.com>
2015-12-10 12:55:58 +0530

    adding script file 15

--- LMS-db/migrations/core_db/V15__added_columns_itemprice_table_and_b_item_master_insertions.sql
+++ LMS-db/migrations/core_db/V15__added_columns_itemprice_table_and_b_item_master_insertions.sql
@@ -3,11 +3,11 @@ DELIMITER //
 create procedure addcolumns() 
 Begin
 IF NOT EXISTS (
-     SELECT * FROM b_item_master.COLUMNS
+     SELECT * FROM information_schema.COLUMNS
      WHERE COLUMN_NAME = 'item_price'
      and TABLE_NAME = 'b_item_master'
      and TABLE_SCHEMA = DATABASE())THEN
-ALTER TABLE `b_item_master` ADD COLUMN `item_price` decimal NOT NULL AFTER `reorder_level`;
+ALTER TABLE `b_item_master` ADD COLUMN `item_price` DECIMAL(22,6) DEFAULT NOT NULL AFTER `reorder_level`;
 
 END IF;
 END //
