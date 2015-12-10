8f004019f4efaad950b907421f207f60db8f4f9c
ramu12 <rammurthy.lingala@gmail.com>
2015-12-10 12:33:32 +0530

    adding a script file

--- LMS-db/migrations/core_db/V15__added_columns_itemprice_table_and_b_item_master_insertions.sql
+++ LMS-db/migrations/core_db/V15__added_columns_itemprice_table_and_b_item_master_insertions.sql
@@ -7,7 +7,7 @@ IF NOT EXISTS (
      WHERE COLUMN_NAME = 'item_price'
      and TABLE_NAME = 'b_item_master'
      and TABLE_SCHEMA = DATABASE())THEN
-ALTER TABLE `b_item_master` ADD COLUMN `item_price` NOT NULL AFTER `reorder_level`;
+ALTER TABLE `b_item_master` ADD COLUMN `item_price` decimal NOT NULL AFTER `reorder_level`;
 
 END IF;
 END //
